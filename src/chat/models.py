a95cbaf09763c50e92b97cb9838f93ad4a782dbb
Sherif Ragimov <ragimov700@yandex.ru>
2024-03-23 01:07:44 +0400

    chat models

--- src/chat/models.py
+++ src/chat/models.py
@@ -1,17 +1,42 @@
 from django.db import models
 
 
+class ChatSession(models.Model):
+    """
+    Модель сессии чата. Сессия может исходить от различных источников
+    и содержит информацию о времени её создания и последнего обновления.
+    """
+    SOURCE_CHOICES = [
+        ('TG', 'Telegram'),
+        ('WA', 'WhatsApp'),
+        ('WEB', 'Web'),
+    ]
+    session_id = models.CharField(max_length=100, verbose_name='идентификатор')
+    source = models.CharField(
+        max_length=3,
+        choices=SOURCE_CHOICES,
+        verbose_name='источник'
+    )
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name='дата создания')
+    updated_at = models.DateTimeField(auto_now=True,
+                                      verbose_name='дата последнего изменения')
+
+
 class ChatMessage(models.Model):
-    user_id = models.CharField(max_length=255)
-    message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    is_user_message = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.message
-
-    class Meta:
-        indexes = [
-            models.Index(fields=['user_id']),
-            models.Index(fields=['created_at'])
-        ]
+    """
+    Модель сообщения в сессии чата. Связано с моделью ChatSession и содержит
+    информацию о тексте сообщения, его отправителе и времени отправки.
+    """
+    session = models.ForeignKey(
+        ChatSession,
+        on_delete=models.CASCADE,
+        related_name='messages',
+        db_index=True,
+        verbose_name='сессия'
+    )
+    message = models.TextField(verbose_name='текст сообщения')
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name='дата отправки')
+    is_user_message = models.BooleanField(default=True,
+                                          verbose_name='отправлено пользователем')
