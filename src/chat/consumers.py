a554cffa08216a3385dc36f561ca2241d7c00e03
Sherif Ragimov <ragimov700@yandex.ru>
2024-03-28 21:11:41 +0400

    websocket uuid

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import uuid
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
@@ -7,6 +8,13 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
+
+        session_id = self.scope['cookies'].get('session_id')
+        if not session_id:
+            session_id = str(uuid.uuid4())
+
+        self.session_id = session_id
+        await self.send(text_data=json.dumps({'session_id': self.session_id}))
 
     async def disconnect(self, close_code):
         pass
@@ -16,5 +24,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = text_data_json['message']
         await asyncio.sleep(2)
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'sessin_id': self.session_id
         }))
