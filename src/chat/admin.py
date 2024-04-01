84d37ee493629b38cfec03c9e17e443435be2929
Sherif Ragimov <ragimov700@yandex.ru>
2024-04-01 19:52:15 +0400

    admin config

--- src/chat/admin.py
+++ src/chat/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
+
 from chat.models import ChatMessage, ChatSession
+
+
+class ChatMessageInline(admin.StackedInline):
+    model = ChatMessage
+    extra = 0
+    fields = ('message', 'created_at', 'is_user_message')
+    readonly_fields = ('message', 'created_at', 'is_user_message')
+    can_delete = False
+
 
 @admin.register(ChatSession)
 class ChatSessionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('session_id', 'source', 'created_at')
+    inlines = [ChatMessageInline]
 
 
 @admin.register(ChatMessage)
 class ChatMessageAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('session', 'message_preview',
+                    'created_at', 'is_user_message')
+
+    def message_preview(self, obj):
+        return obj.message[:50]
+
+    message_preview.short_description = 'Предпросмотр сообщения'
--- src/chat/urls.py
+++ src/chat/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+
 from chat.views import ChatHistory
+
 urlpatterns = [
     path('chat-history/<str:session_id>/', ChatHistory.as_view())
 ]
