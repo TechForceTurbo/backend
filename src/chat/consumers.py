c7dd35293fb00b2b10262062b1712ea813842bf9
Sherif Ragimov <ragimov700@yandex.ru>
2024-04-03 00:04:20 +0400

    save messages to db

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -1,19 +1,22 @@
 import asyncio
 import json
 import uuid
+from urllib.parse import parse_qs
 
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+from chat.models import ChatSession, ChatMessage
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        await self.accept()
-
-        session_id = self.scope['cookies'].get('session_id')
-        if not session_id:
-            session_id = str(uuid.uuid4())
-
+        query_string = self.scope['query_string'].decode()
+        params = parse_qs(query_string)
+        session_id = params.get('session_id', [str(uuid.uuid4())])[0]
         self.session_id = session_id
+        self.session = await self.get_or_create_session()
+        await self.accept()
         await self.send(text_data=json.dumps({'session_id': self.session_id}))
 
     async def disconnect(self, close_code):
@@ -22,8 +25,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        await self.save_message(message)
         await asyncio.sleep(2)
-        await self.send(text_data=json.dumps({
-            'message': message,
-            'sessin_id': self.session_id
-        }))
+        bot_response = message[::-1]
+        await self.save_message(bot_response, is_user_message=False)
+        await self.send(text_data=json.dumps({'message': bot_response}))
+
+    @database_sync_to_async
+    def get_or_create_session(self, source='WEB'):
+        session, _ = ChatSession.objects.get_or_create(
+            session_id=self.session_id, source=source
+        )
+        return session
+
+    @database_sync_to_async
+    def save_message(self, message, is_user_message=True):
+        return ChatMessage.objects.create(
+            session=self.session,
+            message=message,
+            is_user_message=is_user_message
+        )
--- src/config/asgi.py
+++ src/config/asgi.py
@@ -1,9 +1,11 @@
 import os
-from chat.routing import websocket_urlpatterns
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
